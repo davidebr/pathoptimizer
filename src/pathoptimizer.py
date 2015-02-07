3836d5ade52b88c0f47f8361827dcb5d1c3e9d4d
Davide Branduardi <davide.branduardi@gmail.com>
2015-02-08 00:00:34 +0100

    cleanup for flexible pythonic input

--- src/pathoptimizer.py
+++ src/pathoptimizer.py
@@ -15,22 +15,98 @@ import tarfile
 #
 numpy.set_printoptions(precision=4,suppress=True,linewidth=120)
 ##################################################################################
+# convenience function : just paste variables names and content from an environment 
+# to another: useful to parse a pythonic input and spread the content into
+# the various objects
+##################################################################################
+def copyVarsToObjectByName(sinkobject,listvars,sourceobject):
+	""" copy variables from an object to the next based on a list of argument names """
+	for var in listvars:
+		try:
+			getattr(sourceobject, var)
+		except NameError:
+			print "Name ",var," not stated in input and this is needed"	
+			sys.exit(2)
+		setattr(sinkobject,var,getattr(sourceobject, var))
+		print "  Variable \""+var+"\" acquired from input: ",getattr(sinkobject,var)
+##################################################################################
+# convenience pdb reader
+##################################################################################
+def readPDB(filename):
+	""" a simple pdb reader in terms of images """
+	#create an image for each pdb
+	print "Opening file: ",filename
+	f=open(filename,"r")
+	images=[]
+	pdb=[]
+	for line in f:
+		m=line.split()
+		if  m[0]=="END" or m[0]=="TER":
+			# push the pdb into the image
+			#print pdb
+			img=ImageClass()			
+			for i in range(len(pdb)):
+				img.details.append(pdb[i][0])
+				img.x.append(pdb[i][1])
+				img.y.append(pdb[i][2])
+				img.z.append(pdb[i][3])
+				img.occ.append(pdb[i][4])
+				img.beta.append(pdb[i][5])
+			images.append(img)
+			pdb=[]
+		else:
+			if line.startswith('ATOM') or line.startswith('HETATM'):
+				pdb.append( [line[0:30],float(line[31:38]),float(line[39:46]),float(line[47:54]),float(line[54:59]),float(line[60:66])] )	
+	f.close()
+	print "File read! Found ",len(images)," images ! "
+	return images
+
+##################################################################################
+# simple image class
+##################################################################################
+class ImageClass:
+	fake=0
+	x=[]
+	y=[]
+	z=[]
+	occ=[]
+	beta=[]
+	details=[]
+##################################################################################
+# the string is a collection of images plus information on how to run it
+# which dir, soma/pcv , spring constant(s)
+# and all the various plumed-related things
 ##################################################################################
 class StringClass:
-	
-	def optimizationType(self,inputlines):
+
+	def __init__(self,inputvariables):
+		""" read the string from the inputlines"""
+
+		# these are the properties required for this input: if you need any new, just plug it in here: checks are automatically performed
+		needednames=["string","storedir","workdir","dumpfreq","evolstep","discard","maxrounds","optends"]
+
+		print "reading input file for images"
+		self.optends=False
+		copyVarsToObjectByName(self,needednames,inputvariables)
+		# use the variables from here
+       	 	self.imagelist=readPDB(self.string)
+		# type of optimization: soma or pathcvs
+		self.optimizationType(inputvariables)
+       	 	self.rootdir=os.getcwd()
+
+	def optimizationType(self,inputvariables):
 		""" scan the inputlines and see if you have Soma or Pathcvs: then parse the correct arguments """
-                for line in self.inputlines:		
-			m=line.split()	
-			if m[0]=="pathtype": pathtype=m[1]
-                try:
-                        stringinputstring
-                except NameError:
-                        print("ERROR: string not set")
-                        sys.exit(2)	
-	
-	
-	
+		copyVarsToObjectByName(self,["pathtype"],inputvariables)		
+		if  getattr(self,"pathtype").upper()=="SOMA" : 
+			print "This is a SOMA run: need a single spring constant "	
+	                copyVarsToObjectByName(self,["springconstant"],inputvariables)
+		elif  getattr(self,"pathtype").upper()=="PCV" :
+                        print "This is a PCV run "
+	                copyVarsToObjectByName(self,["springconstant_s"],inputvariables)
+	                copyVarsToObjectByName(self,["springconstant_z"],inputvariables)
+		else:
+			print "There is no such pathtype !!! ",getattr(self,"pathtype")
+			sys.exit(2)
 ##################################################################################
 # PathOptimizer object
 ##################################################################################
@@ -76,86 +152,6 @@ class PathOptimizer(object):
         	print "it eventually accepts a -r restart option"
         	sys.exit(0)
 
-	def initString(self):
-		""" read the string from the inputlines"""
-		print "reading input file for images"
-       	 	# parse input
-       	 	optends=False
-       	 	for line in self.inputlines:
-       	 	        	#print line, # note with "," do not print the newline!
-       	 	   	m=line.split()	
-       	 	        if m[0]=="string":
-       	 			inputstring=m[1] 
-       	 	        if m[0]=="storedir":
-       	 			storedir=m[1] 
-       	 	        if m[0]=="workdir":
-       	 			workdir=m[1] 
-       	 	        if m[0]=="dumpfreq":
-       	 			dumpfreq=m[1] 
-       	 	        if m[0]=="evolstep":
-       	 			evolstep=float(m[1]) 
-       	 	        if m[0]=="discard":
-       	 			discard=m[1] 
-       	 	 	if m[0]=="maxrounds":
-       	 			maxrounds=m[1] 
-       	 	 	if m[0]=="optends":
-       	 			optends=True 
-       	 	# do some checks on output
-       	 	#		sys.exit(2)
-       	 	try:
-       	 	 	inputstring	
-       	 	except NameError:
-       	 		print("ERROR: string not set")
-       	 		sys.exit(2)
-       	 	try:
-       	 		storedir 
-       	 	except NameError:
-       	 		print("ERROR: storedir not set")
-       	 		sys.exit(2)
-       	 	try:
-       	 		workdir 
-       	 	except NameError:
-       	 		print("ERROR: workdir not set")
-       	 		sys.exit(2)
-       	 	try:
-       	 		dumpfreq 
-       	 	except NameError:
-       	 		print("ERROR: dumpfreq not set")
-       	 		sys.exit(2)
-       	 	try:
-       	 		evolstep 
-       	 	except NameError:
-       	 		print("ERROR: evolstep not set")
-       	 		sys.exit(2)
-       	 	try:
-       	 		discard	
-       	 	except NameError:
-       	 		print("ERROR: discard value not set ")
-       	 		sys.exit(2)
-       	 	try:
-       	 		maxrounds	
-       	 	except NameError:
-       	 		print("ERROR: maxrounds value not set ")
-       	 		sys.exit(2)
-       	 	#
-       	 	# creating the string
-       	 	#
-       	 	myString=StringClass()
-		# the string itself
-       	 	myString.imagelist=readPDB(inputstring)
-		# type of optimization: soma or pathcvs
-		myString.optimizationType(self.inputlines)
-		# the directories
-       	 	myString.storedir=storedir 
-       	 	myString.workdir=workdir 
-       	 	myString.rootdir=os.getcwd()
-       	 	myString.dumpfreq=dumpfreq 
-       	 	myString.evolstep=evolstep 
-       	 	myString.discard=float(discard)
-       	 	myString.maxrounds=int(maxrounds)
-       	 	myString.optends=optends
-       	 	return myString
-
 ##################################################################################
 #  main routine
 ##################################################################################
@@ -163,6 +159,7 @@ def doOptimization(argv):
         print "String method/Path CV optimizer starting point"
 	# embed everything in a object to keep everything clean
 	po=PathOptimizer(argv)	
+	myString= StringClass(po.inputvariables)		
        	# initial loop
         print "************ INITIAL STRING PARSING       *************"
         sys.exit()
